ef571f987ac437de32894355a8b578be2673186b
ferreira-cmk <ferreirarayane2002@gmail.com>
2025-12-16 20:56:08 -0400

    Adiciona rotas - rayane

--- main.go
+++ main.go
@@ -1,19 +1,18 @@
-
 package main
 
 import (
-    "fmt"
-    "net/http"
-
+	"net/http"
 	"gorm.io/driver/sqlite"
-  	"gorm.io/gorm"
+	"gorm.io/gorm"
 )
 
 type Dados struct {
-  gorm.Model
-  Nome string
-  Preco uint
+	gorm.Model
+	Nome  string
+	Preco uint
 }
+
+var db *gorm.DB
 
 func hello(w http.ResponseWriter, req *http.Request) {
     fmt.Fprintf(w, "hello\n")
@@ -28,20 +27,30 @@ func headers(w http.ResponseWriter, req *http.Request) {
     }
 }
 
-func main() {
-	 
-	db, err := gorm.Open(sqlite.Open("inventario.db"), &gorm.Config{})
-	if err != nil {
-		panic(err) 
-	}
+func listarDados(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "templates/index.html")
+}
 
+func novo(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "templates/form.html")
+}
+
+func main() {
+
+	var err error
+	db, err = gorm.Open(sqlite.Open("inventario.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
 
 	if err := db.AutoMigrate(&Dados{}); err != nil {
 		panic("Erro no automigrate")
 	}
-
     http.HandleFunc("/hello", hello)
     http.HandleFunc("/headers", headers)
 
-    http.ListenAndServe(":8090", nil)
+	http.HandleFunc("/", listarDados)
+	http.HandleFunc("/novo", novo)
+
+	http.ListenAndServe(":8090", nil)
 }
